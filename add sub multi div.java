23bb083437403530e465e63f70ffe2473aecbd0c
Janakiraman <116705319+Janakiraman-J@users.noreply.github.com>
2022-11-09 12:31:07 +0530

    Add files via upload

---
+++ add sub multi div.java
@@ -0,0 +1,19 @@
+public class Test_Modularity {
+ static void add(int a, int b ) {
+	 System.out.println(+(a+b));//Addition
+ }
+ static void sub(int a, int b ) {
+	 System.out.println(+(a-b));//
+ }
+ static void mul(int a, int b ) {
+	 System.out.println(+(a*b));// Multiplication
+ }
+ static void div(int a, int b ) {
+	 System.out.println(+(a/b));//division
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(25,10); sub(25,9); 	mul(9,18);
+		div(100,25);
+		}
+}
