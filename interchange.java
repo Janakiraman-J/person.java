7e1f90f71caac939ad33b6f00ea739122ef3d1a8
Janakiraman <116705319+Janakiraman-J@users.noreply.github.com>
2022-11-09 12:42:41 +0530

    Add files via upload

---
+++ interchange.java
@@ -0,0 +1,11 @@
+public class Test {
+public static void main(String args[]) {
+ int a=10,b=20;
+ int temp=0;
+ System.out.println(+a+" "+b);
+ temp=a; 
+ a=b;
+ b=temp;
+ System.out.println(+a+" "+b); 
+}
+}
---
+++ swapping.java
@@ -0,0 +1,11 @@
+public class Test {
+public static void main(String args[]) {
+ int a=50,b=75;
+ System.out.println(+a+" "+b);
+ System.out.println("After Swap");
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ System.out.println(+a+" "+b); 
+}
+}
